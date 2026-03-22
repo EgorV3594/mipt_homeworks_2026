29ae19b8fded8d4c0f7d88ff06dcfb4d411c4c92
Васильев Егор Кириллович <vasilev.ek@phystech.edu>
2026-03-22 19:26:02 +0300

    fis for last main fix

--- part3_types_conditions_loops_functions/hw3.py
+++ part3_types_conditions_loops_functions/hw3.py
@@ -35,7 +35,7 @@ EXPENSE_CATEGORIES = {
     "Clothing": ("Outerwear", "Casual", "Shoes", "Accessories"),
     "Education": ("Courses", "Books", "Tutors"),
     "Communications": ("Mobile", "Internet", "Subscriptions"),
-    "Other": ("nothing"),
+    "Other": ("SomeCategory", "SomeOtherCategory"),
 }
 
 financial_transactions_storage: list[dict[str, Any]] = []
